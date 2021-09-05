9a5a633acf5026a7dd68ba3198fd596dcba339c7
inhere <in.798@qq.com>
2021-09-05 13:25:04 +0800

    addd smoe new classs

---
+++ src/IO/MemoryIO.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+
+namespace Toolkit\Extlib\IO;
+
+/**
+ * Class MemoryIO
+ * @package Toolkit\Extlib\IO
+ */
+class MemoryIO
+{
+
+}
---
+++ src/IO/StreamIO.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+
+namespace Toolkit\Extlib\IO;
+
+/**
+ * Class StreamIO
+ * @package Toolkit\Extlib\IO
+ */
+class StreamIO
+{
+
+}
